eacebcf17d6fda488736db09393c4033ff88e846
Giovanny Dani Saputra <giovannysaputra14@gmail.com>
2024-04-03 14:20:21 +0700

    Add: common module

---
+++ src/common/common.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ValidationService } from './validation.service';
+import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { APP_FILTER } from '@nestjs/core';
+import { ErrorFilter } from './error.filter';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      transports: [new winston.transports.Console()],
+    }),
+  ],
+  providers: [
+    ValidationService,
+    { provide: APP_FILTER, useClass: ErrorFilter },
+  ],
+})
+export class CommonModule {}
